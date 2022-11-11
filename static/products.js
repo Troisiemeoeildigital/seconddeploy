b0fdf5782580f7fc5e803423a134c1409293a72c
Ehmud bk <dev@neuronarchitecture.com>
2022-11-11 23:17:11 +0100

    added the material sum

--- static/products.js
+++ static/products.js
@@ -503,14 +503,19 @@ buildTable(materialDataUniq)
          <td>${secondMatarrUniq[i].materialGroup}</td>
          <td>${secondMatarrUniq[i].materialName}</td>
          <td>${secondMatarrUniq[i].materialMassg}</td>
-         <td>${recycleRate}</td>
-         <td>${RecycMatg}</td>
-            
+         <td>${recycleRate} %</td>
+         <td>${RecycMatg} (g)</td>
                </tr>
       `
-      	renderEN4557.innerHTML += rowMat
-		}
     
+      	renderEN4557.innerHTML += rowMat
+  
+
+
+		}
+ 
+
+  
   // Summary Energy Recov Weight (g)
 
 
@@ -522,6 +527,76 @@ const energyRecoveryWeightg = document.querySelector('.energyRecoveryWeightg')
 	}
 
    })
+
+.then(()=>{
+      let matTable = document.querySelector('.materialChart')
+      function tableToJson(table) {
+    var data = [];
+
+    // first row needs to be headers
+    var headers = [];
+    for (var i=0; i<table.rows[0].cells.length; i++) {
+        headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,'');
+    }
+
+    // go through cells
+    for (var i=1; i<table.rows.length; i++) {
+
+        var tableRow = table.rows[i];
+        var rowData = {};
+
+        for (var j=0; j<tableRow.cells.length; j++) {
+
+            rowData[ headers[j] ] = tableRow.cells[j].innerHTML;
+
+        }
+
+        data.push(rowData);
+        console.log(data, typeof(data))
+    }       
+ 
+           var resMap = new Map();
+var result = [];
+data.map((x) => {
+    if (!resMap.has(x.materialname))
+        resMap.set(x.materialname, parseFloat(x.recycledmaterialmass));
+    else
+        resMap.set(x.materialname, (parseFloat(x.recycledmaterialmass) + resMap.get(x.materialname)));
+})
+resMap.forEach((value, key) => {
+    result.push({
+        materialname: key,
+        recycledmaterialmass: value
+    })
+})
+console.log(result);
+const summedrecyc = document.querySelector('.summedrecyc')
+summedrecyc.innerHTML = ""
+
+buildTable(result)
+	function buildTable(result){
+
+		for (var i = 0; i < result.length; i++){
+			var recycarr = `<tr>
+          
+							<td>${result[i].materialname}</td>
+              <td>${result[i].recycledmaterialmass}</td>
+           
+                  
+					  </tr>`
+		summedrecyc.innerHTML += recycarr
+
+
+		}
+	}
+
+}
+JSON.stringify(tableToJson(matTable))
+
+
+
+
+})
 
 .then(()=>{
   //filter table by alphabet
@@ -864,7 +939,7 @@ series: [{
     y: materialnum - 1
   }, {
     x: 'Recycled Material(%)',
-    y: parseFloat(sumRecycWeightPerChart.textContent)
+    y: parseFloat(sumValPerc)
   }]
 }]
 }
