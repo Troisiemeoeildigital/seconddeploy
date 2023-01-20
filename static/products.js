bb3b47c6fcaf176fc0038b26c9c3b6c3c6fb51b2
neuronarchitecture <dev@neuronarchitecture.com>
2023-01-20 12:41:35 +0100

    preparing for fb config

--- static/products.js
+++ static/products.js
@@ -2,16 +2,7 @@
 import loadingSkel from './components/products/js/loadingSkelt.js'
 import loadProdTable from './components/products/js/loadProdTable.js'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAq2QjImxRXEtRHN-N6u2YEod-wUJMtI1s",
-  authDomain: "projectcrm-f4e5f.firebaseapp.com",
-  databaseURL: "https://projectcrm-f4e5f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "projectcrm-f4e5f",
-  storageBucket: "projectcrm-f4e5f.appspot.com",
-  messagingSenderId: "404890912341",
-  appId: "1:404890912341:web:5b129be76ccdfeba8c76dc",
-  measurementId: "G-TL8J23TNZE"
-};
+
 
 
 // Initialize Firebase
@@ -280,8 +271,8 @@ window.addEventListener('click', e => {
 
 firebase.auth().onAuthStateChanged(user => {
   const userEmailCard = document.getElementById('useremailcard')
- 
-  userEmailCard.innerHTML = user.email;
+ console.log(user)
+  userEmailCard.innerHTML = user.Email;
   
 })
 
